d50e5b6720f0ecdd5393a2e959c12534516a96c7
Muhammad-Jibril <jbashir52@gmail.com>
2021-02-15 23:33:35 +0800

    add pallet_ethereum to GenesisConfig

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,7 +1,7 @@
 use sp_core::{Pair, Public, sr25519};
 use node_template_runtime::{
 	AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig,
-	SudoConfig, SystemConfig, WASM_BINARY, Signature, TokensConfig, CurrencyId, EVMConfig,
+	SudoConfig, SystemConfig, WASM_BINARY, Signature, TokensConfig, CurrencyId, EVMConfig, EthereumConfig,
 };
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_finality_grandpa::AuthorityId as GrandpaId;
@@ -169,5 +169,6 @@ fn testnet_genesis(
 		pallet_evm: Some(EVMConfig {
 			accounts: BTreeMap::new(),
 		}),
+		pallet_ethereum: Some(EthereumConfig {}),
 	}
 }
