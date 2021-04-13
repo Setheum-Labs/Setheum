f37ea6b9140a8f2dca4731579e77c074a1176860
Muhammad-Jibril <jbashir52@gmail.com>
2021-04-14 04:47:47 +0800

    reformation in-depth

--- scripts/docker-hub-publish-parachain-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-VERSION=$(git rev-parse --short HEAD)
-
-docker build -f scripts/Dockerfile-parachain . -t setheum/setheum-node:pc-$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION}
-docker push setheum/setheum-node:pc-$VERSION
--- scripts/update-mandala.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-# cargo clean
-WASM_BUILD_TYPE=release cargo run --manifest-path bin/setheum-dev/Cargo.toml --features with-ethereum-compatibility -- build-spec --raw --chain newrome-latest > ./resources/newrome-dist.json
