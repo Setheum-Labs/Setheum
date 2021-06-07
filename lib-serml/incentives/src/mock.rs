3695cd50813c19e24f6c71e86cfd80b347908401
Muhammad-Jibril <jbashir52@gmail.com>
2021-06-07 11:03:14 +0800

    mock to fit update

--- lib-serml/incentives/src/mock.rs
+++ lib-serml/incentives/src/mock.rs
@@ -28,7 +28,7 @@ use frame_support::{
 };
 use frame_system::EnsureSignedBy;
 use orml_traits::parameter_type_with_key;
-use primitives::{DexShare, TokenSymbol};
+use primitives::TokenSymbol;
 use sp_core::{H160, H256};
 use sp_runtime::{testing::Header, traits::IdentityLookup};
 use sp_std::cell::RefCell;
@@ -39,15 +39,12 @@ pub type BlockNumber = u64;
 
 pub const ALICE: AccountId = 1;
 pub const BOB: AccountId = 2;
-pub const VAULT: AccountId = 10;
-pub const VALIDATOR: AccountId = 3;
 pub const DNAR: CurrencyId = CurrencyId::Token(TokenSymbol::DNAR);
+pub const SDEX: CurrencyId = CurrencyId::Token(TokenSymbol::SDEX);
 pub const SETT: CurrencyId = CurrencyId::Token(TokenSymbol::SETT);
 pub const USDJ: CurrencyId = CurrencyId::Token(TokenSymbol::USDJ);
-pub const CHFJ_USDJ_LP: CurrencyId =
-	CurrencyId::DexShare(DexShare::Token(TokenSymbol::CHFJ), DexShare::Token(TokenSymbol::USDJ));
-pub const DNAR_USDJ_LP: CurrencyId =
-	CurrencyId::DexShare(DexShare::Token(TokenSymbol::DNAR), DexShare::Token(TokenSymbol::USDJ));
+pub const CHFJ_USDJ_LP: CurrencyId = CurrencyId::DexShare(TokenSymbol::CHFJ, TokenSymbol::USDJ);
+pub const DNAR_USDJ_LP: CurrencyId = CurrencyId::DexShare(TokenSymbol::DNAR, TokenSymbol::USDJ);
 
 mod incentives {
 	pub use super::super::*;
@@ -102,70 +99,119 @@ impl orml_tokens::Config for Runtime {
 
 pub struct MockSerpTreasury;
 impl SerpTreasury<AccountId> for MockSerpTreasury {
+	type Amount = Amount;
 	type Balance = Balance;
 	type CurrencyId = CurrencyId;
+	type BlockNumber = BlockNumber;
+
+	fn get_adjustment_frequency() -> BlockNumber {
+		unimplemented!()
+	}
 
 	fn get_serplus_pool() -> Balance {
 		unimplemented!()
 	}
 
-	fn get_total_setter(_: CurrencyId) -> Balance {
+	fn get_total_setter() -> Balance {
 		unimplemented!()
 	}
 
-	fn get_standard_proportion(_: Balance) -> Ratio {
+	fn get_standard_proportion(_: Balance, _: CurrencyId) -> Ratio {
 		unimplemented!()
 	}
 
-	fn on_system_serpdown(_: Balance) -> DispatchResult {
+	fn get_serplus_serpup(_: Balance, _: CurrencyId) -> DispatchResult {
 		unimplemented!()
 	}
 
-	fn on_system_serpup(_: Balance) -> DispatchResult {
+	fn get_settpay_serpup(_: Balance, _: CurrencyId) -> DispatchResult {
 		unimplemented!()
 	}
 
-	fn issue_standard(who: &AccountId, standard: Balance) -> DispatchResult {
-		TokensModule::deposit(USDJ, who, standard)
-	}
-
-	fn burn_standard(_: &AccountId, _: Balance) -> DispatchResult {
+	fn get_treasury_serpup(_: Balance, _: CurrencyId) -> DispatchResult {
 		unimplemented!()
 	}
 
-	fn issue_propper(who: &AccountId, propper: Balance) -> DispatchResult {
-		TokensModule::deposit(USDJ, who, propper)
-	}
-
-	fn burn_propper(_: &AccountId, _: Balance) -> DispatchResult {
+	fn get_sif_serpup(_: Balance, _: CurrencyId) -> DispatchResult {
 		unimplemented!()
 	}
 
-	fn issue_setter(who: &AccountId, setter: Balance) -> DispatchResult {
-		TokensModule::deposit(SETT, who, setter)
-	}
-
-	fn burn_setter(_: &AccountId, _: Balance) -> DispatchResult {
+	fn get_charity_fund_serpup(_: Balance, _: CurrencyId) -> DispatchResult {
 		unimplemented!()
 	}
 
-	fn issue_dexer(who: &AccountId, dexer: Balance) -> DispatchResult {
-		TokensModule::deposit(SDEX, who, dexer)
-	}
-
-	fn burn_dexer(_: &AccountId, _: Balance) -> DispatchResult {
+	fn on_system_serpup(_: CurrencyId, _: Balance) -> DispatchResult {
 		unimplemented!()
 	}
 
-	fn deposit_serplus(_: &AccountId, _: Balance) -> DispatchResult {
+	fn on_serpup(_: CurrencyId, _: Amount) -> DispatchResult {
 		unimplemented!()
 	}
 
-	fn deposit_reserve(_: &AccountId, _: CurrencyId, _: Balance) -> DispatchResult {
+	fn on_system_serpdown(_: CurrencyId, _: Balance) -> DispatchResult {
 		unimplemented!()
 	}
 
-	fn withdraw_reserve(_: &AccountId, _: CurrencyId, _: Balance) -> DispatchResult {
+	fn on_serpdown(_: CurrencyId, _: Amount) -> DispatchResult {
+		unimplemented!()
+	}
+
+	fn on_serp_tes() -> DispatchRsult {
+		unimplemented!()
+	}
+
+	fn serp_tes(_: CurrencyId) -> DispatchResult {
+		unimplemented!()
+	}
+
+	fn check_all_stablecoin_stability() -> DispatchResult {
+		unimplemented!()
+	}
+
+	fn issue_standard(_: CurrencyId, _: AccountId, _: Balance) -> DispatchResult {
+		unimplemented!()
+	}
+
+	fn burn_standard(_: CurrencyId, _: AccountId, _: Balance) -> DispatchResult {
+		unimplemented!()
+	}
+
+	fn issue_propper(_: CurrencyId, _: AccountId, _: Balance) -> DispatchResult {
+		unimplemented!()
+	}
+
+	fn burn_propper(_: CurrencyId, _: AccountId, _: Balance) -> DispatchResult {
+		unimplemented!()
+	}
+	fn issue_setter(_: AccountId, _: Balance) -> DispatchResult {
+		unimplemented!()
+	}
+
+	fn burn_setter(_: AccountId, _:Balance) -> DispatchResult {
+		unimplemented!()
+	}
+
+	fn issue_dexer(_: AccountId, _:Balance) -> DispatchResult {
+		unimplemented!()
+	}
+
+	fn burn_dexer(_: AccountId, _:Balance) -> DispatchResult {
+		unimplemented!()
+	}
+
+	fn deposit_serplus(_: CurrencyId, _: AccountId, _: Balance) -> DispatchResult {
+		unimplemented!()
+	}
+
+	fn deposit_reserve(_: AccountId, _:Balance) -> DispatchResult {
+		unimplemented!()
+	}
+
+	fn burn_reserve(_: AccountId, _:Balance) -> DispatchResult {
+		unimplemented!()
+	}
+
+	fn withdraw_reserve(_: AccountId, _:Balance) -> DispatchResult {
 		unimplemented!()
 	}
 }
@@ -180,10 +226,6 @@ impl DEXManager<AccountId, CurrencyId, Balance> for MockSetheumDex {
 			(DNAR, SETT) => (100, 400),
 			_ => (0, 0),
 		}
-	}
-
-	fn get_liquidity_token_address(_currency_id_a: CurrencyId, _currency_id_b: CurrencyId) -> Option<H160> {
-		unimplemented!()
 	}
 
 	fn get_swap_target_amount(_: &[CurrencyId], _: Balance, _: Option<Ratio>) -> Option<Balance> {
@@ -226,16 +268,21 @@ impl DEXManager<AccountId, CurrencyId, Balance> for MockSetheumDex {
 impl orml_rewards::Config for Runtime {
 	type Share = Balance;
 	type Balance = Balance;
-	type PoolId = PoolId<AccountId>;
+	type PoolId = PoolId;
 	type Handler = IncentivesModule;
+	type WeightInfo = ();
 }
 
 parameter_types! {
-	pub const RewardsVaultAccountId: AccountId = VAULT;
-	pub const DexIncentivePool: AccountId = 11;
+	pub const DexIncentivePool: AccountId = 10;
+	pub const DexPremiumPool: AccountId = 11;
+	pub const DexPlusPool: AccountId = 12;
+	pub const DexBonusPool: AccountId = 13;
 	pub const AccumulatePeriod: BlockNumber = 10;
-	pub const DexCurrencyId: CurrencyId = SDEX;
-	pub const StableCurrencyId: CurrencyId = SETT;
+	pub const IncentiveCurrencyId: CurrencyId = SDEX;
+	pub const PremiumCurrencyId: CurrencyId = SETT;
+	pub const PlusCurrencyId: CurrencyId = USDJ;
+	pub const BonusCurrencyId: CurrencyId = SETT;
 	pub const IncentivesPalletId: PalletId = PalletId(*b"dnr/inct");
 }
 
@@ -275,28 +322,14 @@ construct_runtime!(
 	}
 );
 
-pub struct ExtBuilder {
-	endowed_accounts: Vec<(AccountId, CurrencyId, Balance)>,
-}
-
-impl Default for ExtBuilder {
-	fn default() -> Self {
-		Self {
-			endowed_accounts: vec![(DNAR, 10_000)],
-		}
-	}
-}
+#[derive(Default)]
+pub struct ExtBuilder;
 
 impl ExtBuilder {
 	pub fn build(self) -> sp_io::TestExternalities {
-		let mut t = frame_system::GenesisConfig::default()
+		let t = frame_system::GenesisConfig::default()
 			.build_storage::<Runtime>()
 			.unwrap();
-		orml_tokens::GenesisConfig::<Runtime> {
-			endowed_accounts: self.endowed_accounts,
-		}
-		.assimilate_storage(&mut t)
-		.unwrap();
 		t.into()
 	}
 }
