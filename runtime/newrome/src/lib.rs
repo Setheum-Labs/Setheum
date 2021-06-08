ab94fc1164340bc69811a43f88d8402b51f57ffa
Muhammad-Jibril <jbashir52@gmail.com>
2021-06-09 05:01:10 +0800

    update `spec_version`

--- runtime/newrome/src/lib.rs
+++ runtime/newrome/src/lib.rs
@@ -104,7 +104,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("newrome"),
 	impl_name: create_runtime_str!("newrome"),
 	authoring_version: 1,
-	spec_version: 709,
+	spec_version: 1,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
--- runtime/setheum/src/lib.rs
+++ runtime/setheum/src/lib.rs
@@ -103,7 +103,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("setheum"),
 	impl_name: create_runtime_str!("setheum"),
 	authoring_version: 1,
-	spec_version: 100,
+	spec_version: 1,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
