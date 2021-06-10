7f5180d2d0f62e33e363ee1c73edca98c079e2b6
Muhammad-Jibril <jbashir52@gmail.com>
2021-06-11 03:32:30 +0800

    Change nightly in scripts

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,8 +5,8 @@ set -e
 echo "*** Initializing WASM build environment"
 
 if [ -z $CI ] ; then
-   rustup update nightly-2021-06-09
+   rustup update nightly-2021-03-15 # 2021-03-15
 fi
 
-rustup target add wasm32-unknown-unknown --toolchain nightly-2021-06-09
-rustup default nightly-2021-06-09
+rustup target add wasm32-unknown-unknown --toolchain nightly-2021-03-15
+rustup default nightly-2021-03-15 # 2021-03-15
