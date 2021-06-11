5393ab3807a8a0ff92bb35a1431e32eefe96631a
Muhammad-Jibril <jbashir52@gmail.com>
2021-06-11 15:08:50 +0800

    update init

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,8 +5,8 @@ set -e
 echo "*** Initializing WASM build environment"
 
 if [ -z $CI ] ; then
-   rustup update nightly-2021-03-14 # 2021-03-14
+   rustup update nightly # 2021-03-14
 fi
 
-rustup target add wasm32-unknown-unknown --toolchain nightly-2021-03-14
-rustup default nightly-2021-03-14 # 2021-03-14
+rustup target add wasm32-unknown-unknown --toolchain nightly  #2021-03-14
+rustup default nightly # 2021-03-14
