be2fddc8df99769d4f653f6bed9cc32b68c6c933
Muhammad-Jibril <jbashir52@gmail.com>
2021-06-21 13:09:49 +0800

    Update Serpups

--- lib-serml/serp-treasury/src/mock.rs
+++ lib-serml/serp-treasury/src/mock.rs
@@ -26,7 +26,7 @@ use frame_system::EnsureSignedBy;
 use orml_traits::parameter_type_with_key;
 use primitives::{TokenSymbol, TradingPair};
 use sp_core::H256;
-use sp_runtime::{testing::Header, traits::IdentityLookup};
+use sp_runtime::{Permill, testing::Header, traits::IdentityLookup};
 use sp_std::cell::RefCell;
 
 pub type AccountId = u128;
@@ -266,25 +266,15 @@ parameter_types! {
 		ZARJ,
 	];
 	pub const GetSetterCurrencyId: CurrencyId = SETT;  // Setter  currency ticker is SETT
-	pub const GetSettUSDCurrencyId: CurrencyId = USDJ; // SettUSD currency ticker is USDJ
-	pub const GetSettGBPCurrencyId: CurrencyId = GBPJ; // SettGBP currency ticker is GBPJ
-	pub const GetSettEURCurrencyId: CurrencyId = EURJ; // SettEUR currency ticker is EURJ
-	pub const GetSettKWDCurrencyId: CurrencyId = KWDJ; // SettKWD currency ticker is KWDJ
-	pub const GetSettJODCurrencyId: CurrencyId = JODJ; // SettJOD currency ticker is JODJ
-	pub const GetSettBHDCurrencyId: CurrencyId = BHDJ; // SettBHD currency ticker is BHDJ
-	pub const GetSettKYDCurrencyId: CurrencyId = KYDJ; // SettKYD currency ticker is KYDJ
-	pub const GetSettOMRCurrencyId: CurrencyId = OMRJ; // SettOMR currency ticker is OMRJ
-	pub const GetSettCHFCurrencyId: CurrencyId = CHFJ; // SettCHF currency ticker is CHFJ
-	pub const GetSettGIPCurrencyId: CurrencyId = GIPJ; // SettGIP currency ticker is GIPJ
 	pub const GetDexerCurrencyId: CurrencyId = SDEX; // SettinDEX currency ticker is SDEX
 
 	pub const SerpTreasuryPalletId: PalletId = PalletId(*b"set/settmintt");
 	pub SerpTesSchedule: BlockNumber = 60; // Triggers SERP-TES for serping after Every 60 blocks
-	pub SerplusSerpupRatio: Rate = Rate::saturating_from_rational(1 : 10); // 10% of SerpUp to buy back & burn NativeCurrency.
-	pub SettPaySerpupRatio: Rate = Rate::saturating_from_rational(6 : 10); // 60% of SerpUp to SettPay as Cashdrops.
-	pub SetheumTreasurySerpupRatio: Rate = Rate::saturating_from_rational(1 : 10); // 10% of SerpUp to network Treasury.
-	pub CharityFundSerpupRatio: Rate = Rate::saturating_from_rational(1 : 10); // 10% of SerpUp to Setheum Foundation's Charity Fund.
-	pub SIFSerpupRatio: Rate = Rate::saturating_from_rational(1 : 10); // 10% of SerpUp to Setheum Investment Fund (SIF) (NIF in Neom).
+	pub SerplusSerpupRatio: Permill = Permill::from_percent(10); // 10% of SerpUp to buy back & burn NativeCurrency.
+	pub SettPaySerpupRatio: Permill = Permill::from_percent(60); // 60% of SerpUp to SettPay as Cashdrops.
+	pub SetheumTreasurySerpupRatio: Permill = Permill::from_percent(10); // 10% of SerpUp to network Treasury.
+	pub CharityFundSerpupRatio: Permill = Permill::from_percent(10); // 10% of SerpUp to Setheum Foundation's Charity Fund.
+	pub SIFSerpupRatio: Rate = Permill = Permill::from_percent(10); // 10% of SerpUp to Setheum Investment Fund (SIF) (NIF in Neom).
 }
 
 impl Config for Runtime {
@@ -292,16 +282,6 @@ impl Config for Runtime {
 	type Currency = Currencies;
 	type StableCurrencyIds = StableCurrencyIds;
 	type GetSetterCurrencyId = GetSetterCurrencyId;
-	type GetSettUSDCurrencyId = GetSettUSDCurrencyId;
-	type GetSettGBPCurrencyId = GetSettGBPCurrencyId;
-	type GetSettEURCurrencyId = GetSettEURCurrencyId;
-	type GetSettKWDCurrencyId = GetSettKWDCurrencyId;
-	type GetSettJODCurrencyId = GetSettJODCurrencyId;
-	type GetSettBHDCurrencyId = GetSettBHDCurrencyId;
-	type GetSettKYDCurrencyId = GetSettKYDCurrencyId;
-	type GetSettOMRCurrencyId = GetSettOMRCurrencyId;
-	type GetSettCHFCurrencyId = GetSettCHFCurrencyId;
-	type GetSettGIPCurrencyId = GetSettGIPCurrencyId;
 	type GetDexerCurrencyId = GetDexerCurrencyId;
 	type SerpTesSchedule = SerpTesSchedule;
 	type SerplusSerpupRatio = SerplusSerpupRatio;
