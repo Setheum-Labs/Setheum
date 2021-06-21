8b1521f613094c6042e26199d1dda3bcee53d556
Muhammad-Jibril <jbashir52@gmail.com>
2021-06-22 01:06:48 +0800

    update

--- lib-serml/currencies/src/weights.rs
+++ lib-serml/currencies/src/weights.rs
@@ -24,7 +24,6 @@
 //! EXECUTION: Some(Wasm), WASM-EXECUTION: Compiled, CHAIN: Some("dev"), DB CACHE: 128
 
 // Executed Command:
-// /Users/xiliangchen/projects/acala/target/release/acala
 // benchmark
 // --chain=dev
 // --steps=50
