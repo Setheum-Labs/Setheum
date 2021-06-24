1dffc8e20ab1c92e7a642b00cc391f2ae29f0caa
Muhammad-Jibril <jbashir52@gmail.com>
2021-06-25 00:47:11 +0800

    pdate settway mock

--- lib-serml/setters-manager/src/mock.rs
+++ lib-serml/setters-manager/src/mock.rs
@@ -26,7 +26,10 @@ use frame_system::EnsureSignedBy;
 use orml_traits::parameter_type_with_key;
 use primitives::TokenSymbol;
 use sp_core::H256;
-use sp_runtime::{testing::Header, traits::IdentityLookup};
+use sp_runtime::{
+	testing::Header,
+	traits::{AccountIdConversion, IdentityLookup},
+};
 use support::{SerpAuctionManager, StandardValidator};
 
 pub type AccountId = u128;
@@ -112,6 +115,7 @@ impl frame_system::Config for Runtime {
 	type BaseCallFilter = ();
 	type SystemWeightInfo = ();
 	type SS58Prefix = ();
+	type OnSetCode = ();
 }
 
 parameter_type_with_key! {
