120874078651a9a0304e51325b93f7282b5b1854
Muhammad-Jibril <jbashir52@gmail.com>
2021-06-25 11:52:00 +0800

    Remove ARS and UAH

--- lib-serml/serp-auction/src/mock.rs
+++ lib-serml/serp-auction/src/mock.rs
@@ -47,7 +47,6 @@ pub const DNAR: CurrencyId = CurrencyId::Token(TokenSymbol::DNAR);
 pub const SDEX: CurrencyId = CurrencyId::Token(TokenSymbol::SDEX);
 pub const SETT: CurrencyId = CurrencyId::Token(TokenSymbol::SETT);
 pub const AEDJ: CurrencyId = CurrencyId::Token(TokenSymbol::AEDJ);
-pub const ARSJ: CurrencyId = CurrencyId::Token(TokenSymbol::ARSJ);
 pub const AUDJ: CurrencyId = CurrencyId::Token(TokenSymbol::AUDJ);
 pub const BRLJ: CurrencyId = CurrencyId::Token(TokenSymbol::BRLJ);
 pub const CADJ: CurrencyId = CurrencyId::Token(TokenSymbol::CADJ);
@@ -83,7 +82,6 @@ pub const THBJ: CurrencyId = CurrencyId::Token(TokenSymbol::THBJ);
 pub const TRYJ: CurrencyId = CurrencyId::Token(TokenSymbol::TRYJ);
 pub const TWDJ: CurrencyId = CurrencyId::Token(TokenSymbol::TWDJ);
 pub const TZSJ: CurrencyId = CurrencyId::Token(TokenSymbol::TZSJ);
-pub const UAHJ: CurrencyId = CurrencyId::Token(TokenSymbol::UAHJ);
 pub const USDJ: CurrencyId = CurrencyId::Token(TokenSymbol::USDJ);
 pub const ZARJ: CurrencyId = CurrencyId::Token(TokenSymbol::ZARJ);
 
@@ -153,7 +151,6 @@ parameter_types! {
 	pub StableCurrencyIds: Vec<CurrencyId> = vec![
 		SETT,
 		AEDJ,
-		ARSJ,
  		AUDJ,
 		BRLJ,
 		CADJ,
@@ -189,7 +186,6 @@ parameter_types! {
 		TRYJ,
 		TWDJ,
 		TZSJ,
-		UAHJ,
 		USDJ,
 		ZARJ,
 	];
