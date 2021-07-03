75581636a8246d18ba8a38462b24f7a45b2cf153
Muhammad-Jibril <jbashir52@gmail.com>
2021-07-03 18:03:10 +0800

    update precompile tests

--- runtime/common/src/precompile/tests.rs
+++ runtime/common/src/precompile/tests.rs
@@ -60,7 +60,7 @@ pub type WithSystemContractFilter = AllPrecompiles<
 >;
 
 #[test]
-fn precompile_filter_works_on_Setheum_precompiles() {
+fn precompile_filter_works_on_setheum_precompiles() {
 	let precompile = H160::from_low_u64_be(PRECOMPILE_ADDRESS_START);
 
 	let mut non_system = [0u8; 20];
