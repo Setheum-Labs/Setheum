ca3895e07306c9d9c2e748979e7bd80da0159ca4
Muhammad-Jibril <jbashir52@gmail.com>
2021-07-10 18:22:53 +0800

    ...

--- lib-serml/democracy/src/tests/decoders.rs
+++ lib-serml/democracy/src/tests/decoders.rs
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! The for various partial storage decoders
+//! The tests for various partial storage decoders
 
 use super::*;
 use frame_support::storage::{migration, unhashed};
