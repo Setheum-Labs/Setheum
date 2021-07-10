6ceb7c823fe6f9189d31bdf97c71f60a92a183f9
Muhammad-Jibril <jbashir52@gmail.com>
2021-07-10 13:09:19 +0800

    Update benchmarking TODO

--- lib-serml/democracy/src/benchmarking.rs
+++ lib-serml/democracy/src/benchmarking.rs
@@ -15,7 +15,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! Democracy pallet benchmarking.
+//! Setheum Democracy benchmarking.
+// TODO: Update!
 
 use super::*;
 
