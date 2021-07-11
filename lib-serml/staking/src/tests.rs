dfeec7617890b7d8533214d00ee18011b2bfbc75
Muhammad-Jibril <jbashir52@gmail.com>
2021-07-11 10:47:00 +0800

    update tests

--- lib-serml/staking/src/tests.rs
+++ lib-serml/staking/src/tests.rs
@@ -1,6 +1,6 @@
-// This file is part of Substrate.
+// This file is part of Setheum.
 
-// Copyright (C) 2017-2021 Parity Technologies (UK) Ltd.
+// Copyright (C) 2017-2021 Setheum Labs.
 // SPDX-License-Identifier: Apache-2.0
 
 // Licensed under the Apache License, Version 2.0 (the "License");
