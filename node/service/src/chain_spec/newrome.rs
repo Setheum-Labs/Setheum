6850ba3bfe6d170b293708ef84499835c81ce772
Muhammad-Jibril <jbashir52@gmail.com>
2021-07-15 02:54:32 +0800

    wake-up

--- node/service/src/chain_spec/newrome.rs
+++ node/service/src/chain_spec/newrome.rs
@@ -27,7 +27,7 @@ use sp_finality_grandpa::AuthorityId as GrandpaId;
 use sp_runtime::{FixedPointNumber, FixedU128, Perbill};
 
 use crate::chain_spec::{
-	get_account_id_from_seed, get_authority_keys_from_seed, Extensions, TELEMETRY_URL,
+	evm_genesis, get_account_id_from_seed, get_authority_keys_from_seed, Extensions, TELEMETRY_URL,
 };
 
 pub type ChainSpec = sc_service::GenericChainSpec<newrome_runtime::GenesisConfig, Extensions>;
@@ -52,8 +52,8 @@ pub fn development_testnet_config() -> Result<ChainSpec, String> {
 	let wasm_binary = newrome_runtime::WASM_BINARY.unwrap_or_default();
 
 	Ok(ChainSpec::from_genesis(
-		"Development",
-		"dev",
+		"NewRome Dev",
+		"newrome-dev",
 		ChainType::Development,
 		move || {
 			testnet_genesis(
@@ -82,7 +82,6 @@ pub fn development_testnet_config() -> Result<ChainSpec, String> {
 /// Local testnet config (multivalidator Alice + Bob)
 pub fn local_testnet_config() -> Result<ChainSpec, String> {
 	let mut properties = Map::new();
-
 	let mut token_symbol: Vec<String> = vec![];
 	let mut token_decimals: Vec<u32> = vec![];
 	TokenSymbol::get_info().iter().for_each(|(symbol_name, decimals)| {
@@ -91,6 +90,7 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 	});
 	properties.insert("tokenSymbol".into(), token_symbol.into());
 	properties.insert("tokenDecimals".into(), token_decimals.into());
+
 	let wasm_binary = newrome_runtime::WASM_BINARY.ok_or("Dev runtime wasm binary not available")?;
 
 	Ok(ChainSpec::from_genesis(
@@ -100,11 +100,14 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 		move || {
 			testnet_genesis(
 				wasm_binary,
+				// Initial PoA authorities
 				vec![
 					get_authority_keys_from_seed("Alice"),
 					get_authority_keys_from_seed("Bob"),
 				],
+				// Sudo account
 				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				// Pre-funded accounts
 				vec![
 					get_account_id_from_seed::<sr25519::Public>("Alice"),
 					get_account_id_from_seed::<sr25519::Public>("Bob"),
@@ -141,11 +144,11 @@ pub fn latest_newrome_testnet_config() -> Result<ChainSpec, String> {
 	properties.insert("tokenSymbol".into(), token_symbol.into());
 	properties.insert("tokenDecimals".into(), token_decimals.into());
 	
-	let wasm_binary = newrome_runtime::WASM_BINARY.ok_or("Newrome runtime wasm binary not available")?;
+	let wasm_binary = newrome_runtime::WASM_BINARY.ok_or("NewRome runtime wasm binary not available")?;
 
 	Ok(ChainSpec::from_genesis(
-		"Setheum Newrome TC5",
-		"newrome5",
+		"Setheum NewRome",
+		"newrome",
 		ChainType::Live,
 		// SECRET="..."
 		// ./target/debug/subkey inspect "$SECRET//setheum//root"
@@ -162,8 +165,10 @@ pub fn latest_newrome_testnet_config() -> Result<ChainSpec, String> {
 		move || {
 			newrome_genesis(
 				wasm_binary,
+				// Initial PoA authorities
 				vec![
 					(
+						// Authority 1
 						// 5CLg63YpPJNqcyWaYebk3LuuUVp3un7y1tmuV3prhdbnMA77
 						hex!["0c2df85f943312fc853059336627d0b7a08669629ebd99b4debc6e58c1b35c2b"].into(),
 						hex!["0c2df85f943312fc853059336627d0b7a08669629ebd99b4debc6e58c1b35c2b"].into(),
@@ -171,6 +176,7 @@ pub fn latest_newrome_testnet_config() -> Result<ChainSpec, String> {
 						hex!["948f15728a5fd66e36503c048cc7b448cb360a825240c48ff3f89efe050de608"].unchecked_into(),
 					),
 					(
+						// Authority 2
 						// 5FnLzAUmXeTZg5J9Ao5psKU68oA5PBekXqhrZCKDbhSCQi88
 						hex!["a476c0050065dafac1e9ff7bf602fe628ceadacf67650f8317554bd571b73507"].into(),
 						hex!["a476c0050065dafac1e9ff7bf602fe628ceadacf67650f8317554bd571b73507"].into(),
@@ -178,6 +184,7 @@ pub fn latest_newrome_testnet_config() -> Result<ChainSpec, String> {
 						hex!["b4f5713322656d29930aa89efa5509554a36c40fb50a226eae0f38fc1a6ceb25"].unchecked_into(),
 					),
 					(
+						// Authority 3
 						// 5Gn5LuLuWNcY21Vue4QcFFD3hLvjQY3weMHXuEyejUbUnArt
 						hex!["d07e538fee7c42be9b2627ea5caac9a30f1869d65af2a19df70138d5fcc34310"].into(),
 						hex!["d07e538fee7c42be9b2627ea5caac9a30f1869d65af2a19df70138d5fcc34310"].into(),
@@ -185,23 +192,28 @@ pub fn latest_newrome_testnet_config() -> Result<ChainSpec, String> {
 						hex!["4cc54799f38715771605a21e8272a7a1344667e4681611988a913412755a8a04"].unchecked_into(),
 					),
 				],
+				// Sudo account
 				// 5F98oWfz2r5rcRVnP9VCndg33DAAsky3iuoBSpaPUbgN9AJn
 				hex!["8815a8024b06a5b4c8703418f52125c923f939a5c40a717f6ae3011ba7719019"].into(),
+				// Pre-funded accounts
 				vec![
+					// Pre-funded account 1 (same as sudo account here)
 					// 5F98oWfz2r5rcRVnP9VCndg33DAAsky3iuoBSpaPUbgN9AJn
 					hex!["8815a8024b06a5b4c8703418f52125c923f939a5c40a717f6ae3011ba7719019"].into(),
+					// Pre-funded account 2
 					// 5Fe3jZRbKes6aeuQ6HkcTvQeNhkkRPTXBwmNkuAPoimGEv45
 					hex!["9e22b64c980329ada2b46a783623bcf1f1d0418f6a2b5fbfb7fb68dbac5abf0f"].into(),
 				],
 			)
 		},
 		vec![
-			"/dns/testnet-bootnode-1.setheum.laminar.one/tcp/30333/p2p/12D3KooWAFUNUowRqCV4c5so58Q8iGpypVf3L5ak91WrHf7rPuKz"
-				.parse()
-				.unwrap(),
+			// TODO: Update!
+			//"/dns/testnet-bootnode-1.newrome.setheum.xyz/tcp/30333/p2p/12D3KooWAFUNUowRqCV4c5so58Q8iGpypVf3L5ak91WrHf7rPuKz"
+			//	.parse()
+			//	.unwrap(),
 		],
 		TelemetryEndpoints::new(vec![(TELEMETRY_URL.into(), 0)]).ok(),
-		Some("newrome5"),
+		Some("newrome"),
 		Some(properties),
 		Default::default(),
 	))
@@ -220,10 +232,12 @@ fn testnet_genesis(
 	use newrome_runtime::{
 		dollar, get_all_module_accounts, SetheumOracleConfig, BabeConfig, Balance, BalancesConfig,
 		SettmintEngineConfig, SerpTreasuryConfig, DexConfig, EnabledTradingPairs,
-		GeneralCouncilMembershipConfig, GrandpaConfig, FinancialCouncilMembershipConfig,
-		IndicesConfig, NativeTokenExistentialDeposit, OperatorMembershipSetheumConfig,
-		OrmlNFTConfig, RenVmBridgeConfig, SessionConfig, StakerStatus, StakingConfig, SudoConfig,
-		SystemConfig, TechnicalCommitteeMembershipConfig, TokensConfig, VestingConfig, DNAR, SETT, USDJ,
+		GeneralCouncilMembershipConfig, SetheumJuryMembershipConfig, GrandpaConfig,
+		FinancialCouncilMembershipConfig, ExchangeCouncilMembershipConfig, IndicesConfig,
+		NativeTokenExistentialDeposit, OperatorMembershipSetheumConfig, OrmlNFTConfig,
+		RenVmBridgeConfig, SessionConfig, StakerStatus, StakingConfig, SudoConfig,
+		SystemConfig, TechnicalCommitteeMembershipConfig, TokensConfig, VestingConfig,
+		DNAR, DRAM, SETT, USDJ, EURJ, JPYJ, GBPJ, AUDJ, CADJ, CHFJ, SEKJ, SGDJ, SARJ RENBTC,
 	};
 	#[cfg(feature = "std")]
 	use sp_std::collections::btree_map::BTreeMap;
@@ -232,6 +246,8 @@ fn testnet_genesis(
 
 	let initial_balance: u128 = 1_000_000 * dollar(DNAR);
 	let initial_staking: u128 = 100_000 * dollar(DNAR);
+
+	let evm_genesis_accounts = evm_genesis();
 
 	let balances = initial_authorities
 		.iter()
@@ -292,43 +308,81 @@ fn testnet_genesis(
 			phantom: Default::default(),
 		}),
 		pallet_collective_Instance2: Some(Default::default()),
-		pallet_membership_Instance2: Some(FinancialCouncilMembershipConfig {
+		pallet_membership_Instance2: Some(SetheumJuryMembershipConfig {
+			members: vec![root_key.clone()],
+			phantom: Default::default(),
+		}),
+		pallet_collective_Instance3: Some(Default::default()),
+		pallet_membership_Instance3: Some(FinancialCouncilMembershipConfig {
 			members: vec![root_key.clone()],
 			phantom: Default::default(),
 		}),
 		pallet_collective_Instance4: Some(Default::default()),
-		pallet_membership_Instance4: Some(TechnicalCommitteeMembershipConfig {
+		pallet_membership_Instance4: Some(ExchangeCouncilMembershipConfig {
 			members: vec![root_key.clone()],
 			phantom: Default::default(),
 		}),
-		pallet_membership_Instance5: Some(OperatorMembershipSetheumConfig {
+		pallet_collective_Instance5: Some(Default::default()),
+		pallet_membership_Instance5: Some(TechnicalCommitteeMembershipConfig {
 			members: vec![root_key.clone()],
+			phantom: Default::default(),
+		}),
+		pallet_membership_Instance6: Some(OperatorMembershipSetheumConfig {
+			members: endowed_accounts.clone(),
 			phantom: Default::default(),
 		}),
 		pallet_treasury: Some(Default::default()),
 		orml_tokens: Some(TokensConfig {
 			endowed_accounts: endowed_accounts
 				.iter()
-				.flat_map(|x| vec![(x.clone(), DOT, initial_balance), (x.clone(), CHFJ, initial_balance)])
+				.flat_map(|x| vec![
+					(x.clone(), SETT, initial_balance),
+					(x.clone(), USDJ, initial_balance),
+					(x.clone(), EURJ, initial_balance),
+					(x.clone(), JPYJ, initial_balance),
+					(x.clone(), GBPJ, initial_balance),
+					(x.clone(), AUDJ, initial_balance),
+					(x.clone(), CADJ, initial_balance),
+					(x.clone(), CHFJ, initial_balance),
+					(x.clone(), SEKJ, initial_balance),
+					(x.clone(), SGDJ, initial_balance),
+					(x.clone(), SARJ, initial_balance)
+				])
 				.collect(),
 		}),
 		orml_vesting: Some(VestingConfig { vesting: vec![] }),
-		serp_treasury: Some(SerpTreasuryConfig {
-			expected_setter_auction_size: vec![
-				(DOT, dollar(DOT)), // (currency_id, max size of a setter auction)
-				(CHFJ, dollar(CHFJ)),
-				(GBPJ, dollar(GBPJ)),
-			],
-		}),
 		orml_oracle_Instance1: Some(SetheumOracleConfig {
 			members: Default::default(), // initialized by OperatorMembership
 			phantom: Default::default(),
 		}),
-		dex: Some(DexConfig {
+		setheum_dex: DexConfig {
 			initial_listing_trading_pairs: vec![],
 			initial_enabled_trading_pairs: EnabledTradingPairs::get(),
-			initial_added_liquidity_pools: vec![],
-		}),
+			initial_added_liquidity_pools: vec![(
+				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				vec![
+					(TradingPair::new(SETT, DNAR), (1_000_000u128, 2_000_000u128)),
+					(TradingPair::new(SETT, MENA), (1_000_000u128, 2_000_000u128)),
+					(TradingPair::new(SETT, USDJ), (1_000_000u128, 1_606_750u128)),
+					(TradingPair::new(SETT, EURJ), (1_000_000u128, 1_365_737u128)),
+					(TradingPair::new(SETT, JPYJ), (1_000_000u128, 1_767_103u128)),
+					(TradingPair::new(SETT, GBPJ), (1_000_000u128, 1_156_860u128)),
+					(TradingPair::new(SETT, AUDJ), (1_000_000u128, 2_153_045u128)),
+					(TradingPair::new(SETT, CADJ), (1_000_000u128, 2_008_437u128)),
+					(TradingPair::new(SETT, CHFJ), (1_000_000u128, 1_462_142u128)),
+					(TradingPair::new(SETT, SEKJ), (1_000_000u128, 13_850_185u128)),
+					(TradingPair::new(SETT, SGDJ), (1_000_000u128, 2_169_112u128)),
+					(TradingPair::new(SETT, SARJ), (1_000_000u128, 6_025_312u128)),
+				],
+			)],
+		},
+		setheum_evm: EVMConfig {
+			accounts: evm_genesis_accounts,
+			treasury: root_key,
+		},
+		setheum_renvm_bridge: RenVmBridgeConfig {
+			ren_vm_public_key: hex!["4b939fc8ade87cb50b78987b1dda927460dc456a"],
+		},
 		orml_nft: Some(OrmlNFTConfig { tokens: vec![] }),
 	}
 }
@@ -340,13 +394,14 @@ fn newrome_genesis(
 	endowed_accounts: Vec<AccountId>,
 ) -> newrome_runtime::GenesisConfig {
 	use newrome_runtime::{
-		cent, dollar, get_all_module_accounts, SetheumOracleConfig,
-		BabeConfig, Balance, BalancesConfig, SettmintEngineConfig, SerpTreasuryConfig,
-		DexConfig, EnabledTradingPairs, GeneralCouncilMembershipConfig, GrandpaConfig,
-		FinancialCouncilMembershipConfig, IndicesConfig, NativeTokenExistentialDeposit,
-		OperatorMembershipSetheumConfig, OrmlNFTConfig, SessionConfig, StakerStatus,
-		StakingConfig, SudoConfig, SystemConfig, TechnicalCommitteeMembershipConfig,
-		TokensConfig, VestingConfig, DNAR, SETT, USDJ,
+		cent, dollar, get_all_module_accounts, SetheumOracleConfig, BabeConfig, Balance,
+		BalancesConfig, SettmintEngineConfig, SerpTreasuryConfig, DexConfig,
+		EnabledTradingPairs, GeneralCouncilMembershipConfig, SetheumJuryMembershipConfig,
+		GrandpaConfig, FinancialCouncilMembershipConfig, ExchangeCouncilMembershipConfig,
+		IndicesConfig, NativeTokenExistentialDeposit, OperatorMembershipSetheumConfig,
+		OrmlNFTConfig, SessionConfig, StakerStatus, StakingConfig, SudoConfig,
+		SystemConfig, TechnicalCommitteeMembershipConfig, TokensConfig, VestingConfig,
+		DNAR, DRAM, SETT, USDJ, EURJ, JPYJ, GBPJ, AUDJ, CADJ, CHFJ, SEKJ, SGDJ, SARJ RENBTC,
 	};
 	#[cfg(feature = "std")]
 	use sp_std::collections::btree_map::BTreeMap;
@@ -355,6 +410,8 @@ fn newrome_genesis(
 
 	let initial_balance: u128 = 1_000_000 * dollar(DNAR);
 	let initial_staking: u128 = 100_000 * dollar(DNAR);
+
+	let evm_genesis_accounts = evm_genesis();
 
 	let balances = initial_authorities
 		.iter()
@@ -415,42 +472,91 @@ fn newrome_genesis(
 			phantom: Default::default(),
 		}),
 		pallet_collective_Instance2: Some(Default::default()),
-		pallet_membership_Instance2: Some(FinancialCouncilMembershipConfig {
+		pallet_membership_Instance2: Some(SetheumJuryMembershipConfig {
+			members: vec![root_key.clone()],
+			phantom: Default::default(),
+		}),
+		pallet_collective_Instance3: Some(Default::default()),
+		pallet_membership_Instance3: Some(FinancialCouncilMembershipConfig {
 			members: vec![root_key.clone()],
 			phantom: Default::default(),
 		}),
 		pallet_collective_Instance4: Some(Default::default()),
-		pallet_membership_Instance4: Some(TechnicalCommitteeMembershipConfig {
+		pallet_membership_Instance4: Some(ExchangeCouncilMembershipConfig {
 			members: vec![root_key.clone()],
 			phantom: Default::default(),
 		}),
-		pallet_membership_Instance5: Some(OperatorMembershipSetheumConfig {
+		pallet_collective_Instance5: Some(Default::default()),
+		pallet_membership_Instance5: Some(TechnicalCommitteeMembershipConfig {
+			members: vec![root_key.clone()],
+			phantom: Default::default(),
+		}),
+		pallet_membership_Instance6: Some(OperatorMembershipSetheumConfig {
 			members: endowed_accounts.clone(),
 			phantom: Default::default(),
 		}),
 		pallet_treasury: Some(Default::default()),
 		orml_tokens: Some(TokensConfig {
 			endowed_accounts: vec![
-				(root_key.clone(), DOT, initial_balance),
-				(root_key, CHFJ, initial_balance),
+				(root_key.clone(), SETT, initial_balance),
+				(root_key.clone(), USDJ, initial_balance),
+				(root_key.clone(), EURJ, initial_balance),
+				(root_key.clone(), JPYJ, initial_balance),
+				(root_key.clone(), GBPJ, initial_balance),
+				(root_key.clone(), AUDJ, initial_balance),
+				(root_key.clone(), CADJ, initial_balance),
+				(root_key.clone(), CHFJ, initial_balance),
+				(root_key.clone(), SEKJ, initial_balance),
+				(root_key.clone(), SGDJ, initial_balance),
+				(root_key.clone(), SARJ, initial_balance)
 			],
 		}),
 		orml_vesting: Some(VestingConfig { vesting: vec![] }),
-		serp_treasury: Some(SerpTreasuryConfig {
-			expected_setter_auction_size: vec![
-				(DOT, dollar(DOT)), // (currency_id, max size of a setter auction)
-				(CHFJ, 5 * cent(CHFJ)),
-				(GBPJ, 5 * cent(GBPJ)),
-			],
-		}),
 		orml_oracle_Instance1: Some(SetheumOracleConfig {
 			members: Default::default(), // initialized by OperatorMembership
 			phantom: Default::default(),
 		}),
-		dex: Some(DexConfig {
+		setheum_dex: Some(DexConfig {
 			initial_listing_trading_pairs: vec![],
 			initial_enabled_trading_pairs: EnabledTradingPairs::get(),
 			initial_added_liquidity_pools: vec![],
 		}),
+		setheum_evm: EVMConfig {
+			accounts: evm_genesis_accounts,
+			treasury: root_key,
+		},
+		setheum_renvm_bridge: RenVmBridgeConfig {
+			ren_vm_public_key: hex!["4b939fc8ade87cb50b78987b1dda927460dc456a"],
+		},
+		orml_nft: OrmlNFTConfig {
+			tokens: {
+				let nft_airdrop_json = &include_bytes!("../../../../resources/newrome-airdrop-NFT.json")[..];
+				let nft_airdrop: Vec<(
+					AccountId,
+					Vec<u8>,
+					module_nft::ClassData<Balance>,
+					Vec<(Vec<u8>, module_nft::TokenData<Balance>, Vec<AccountId>)>,
+				)> = serde_json::from_slice(nft_airdrop_json).unwrap();
+
+				let mut tokens = vec![];
+				for (class_owner, class_meta, class_data, nfts) in nft_airdrop {
+					let mut tokens_of_class = vec![];
+					for (token_meta, token_data, token_owners) in nfts {
+						token_owners.iter().for_each(|account_id| {
+							tokens_of_class.push((account_id.clone(), token_meta.clone(), token_data.clone()));
+						});
+					}
+
+					tokens.push((
+						class_owner.clone(),
+						class_meta.clone(),
+						class_data.clone(),
+						tokens_of_class,
+					));
+				}
+
+				tokens
+			},
+		},
 	}
 }
