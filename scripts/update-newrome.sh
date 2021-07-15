3595e074b2eafda8d6a56b93305b07e77b000c79
Muhammad-Jibril <jbashir52@gmail.com>
2021-07-15 16:52:55 +0800

    Update update-newrome.sh

--- scripts/update-newrome.sh
+++ scripts/update-newrome.sh
@@ -3,4 +3,4 @@
 set -e
 
 # cargo clean
-WASM_BUILD_TYPE=release cargo run --manifest-path node/setheum-dev/Cargo.toml -- build-spec --raw --chain newrome-latest > ./resources/newrome-dist.json
+WASM_BUILD_TYPE=release cargo run --features with-newrome-runtime --features with-sevm -- build-spec --raw --chain newrome-latest > ./resources/newrome-dist.json
