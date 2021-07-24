ea26a23e9d676145efc30c66b106fc0b2920845a
Muhammad-Jibril <jbashir52@gmail.com>
2021-07-24 22:20:46 +0100

    Update docker-hub-publish-dev.sh

--- scripts/docker-hub-publish-dev.sh
+++ scripts/docker-hub-publish-dev.sh
@@ -3,8 +3,8 @@
 set -e
 
 VERSION=$(git rev-parse --short HEAD)
-NODE_NAME=setheum/mandala-node
-BUILD_ARGS="--features with-mandala-runtime --features=with-sevm"
+NODE_NAME=setheum/newrome-node
+BUILD_ARGS="--features with-newrome-runtime --features=with-sevm"
 
 docker build -f scripts/Dockerfile . -t $NODE_NAME:$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION} --build-arg BUILD_ARGS="$BUILD_ARGS"
 docker push $NODE_NAME:$VERSION
