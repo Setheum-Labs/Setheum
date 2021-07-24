7b3862312b27ece4b2fdb8e4964eaa09f4408da1
Muhammad-Jibril <jbashir52@gmail.com>
2021-07-24 22:06:44 +0100

    Update docker-hub-publish-dev.sh

--- scripts/docker-hub-publish-dev.sh
+++ scripts/docker-hub-publish-dev.sh
@@ -3,7 +3,7 @@
 set -e
 
 VERSION=$(git rev-parse --short HEAD)
-NODE_NAME=acala/mandala-node
+NODE_NAME=setheum/mandala-node
 BUILD_ARGS="--features with-mandala-runtime --features=with-sevm"
 
 docker build -f scripts/Dockerfile . -t $NODE_NAME:$VERSION --no-cache --build-arg GIT_COMMIT=${VERSION} --build-arg BUILD_ARGS="$BUILD_ARGS"
