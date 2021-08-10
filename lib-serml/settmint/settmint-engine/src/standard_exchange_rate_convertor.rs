11527dfbb368655301cf818716555a51a918ba4c
Muhammad-Jibril <jbashir52@gmail.com>
2021-08-10 20:52:07 +0800

    Update standard_exchange_rate_convertor.rs

--- lib-serml/settmint/settmint-engine/src/standard_exchange_rate_convertor.rs
+++ lib-serml/settmint/settmint-engine/src/standard_exchange_rate_convertor.rs
@@ -21,7 +21,7 @@ use primitives::{Balance, CurrencyId};
 use sp_runtime::traits::Convert;
 use sp_runtime::FixedPointNumber;
 
-pub struct StandardExchangeRateConvertor<T>(sp_std::marker::_);
+pub struct StandardExchangeRateConvertor<T>(sp_std::marker::PhantomData<T>);
 
 impl<T> Convert<(CurrencyId, Balance), Balance> for StandardExchangeRateConvertor<T>
 where
