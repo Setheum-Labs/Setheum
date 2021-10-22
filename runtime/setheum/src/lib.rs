d1fbf59cb7042344658e9b75cc24e3030ef80306
Muhammad-Jibril <jbashir52@gmail.com>
2021-10-22 13:38:38 +0800

    Update lib.rs

--- runtime/setheum/src/lib.rs
+++ runtime/setheum/src/lib.rs
@@ -2243,6 +2243,7 @@ impl_runtime_apis! {
 				hex_literal::hex!("26aa394eea5630e07c48ae0c9558cef70a98fdbe9ce6c55837576c60c7af3850").to_vec().into(),
 				// System Events
 				hex_literal::hex!("26aa394eea5630e07c48ae0c9558cef780d41e5e16056765bc8461851072c9d7").to_vec().into(),
+				/// TODO: Update hexcode
 				// Caller 0 Account
 				hex_literal::hex!("26aa394eea5630e07c48ae0c9558cef7b99d880ec681799c0cf30e8886371da946c154ffd9992e395af90b5b13cc6f295c77033fce8a9045824a6690bbf99c6db269502f0a8d1d2a008542d5690a0749").to_vec().into(),
 				// Caller 1 Account
