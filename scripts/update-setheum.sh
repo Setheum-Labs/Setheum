a146ba06d5876aa78c058bbd031822217d8d3cbf
Muhammad-Jibril <jbashir52@gmail.com>
2021-10-23 14:35:56 +0800

    update scripts

--- scripts/update-setheum.sh
+++ scripts/update-setheum.sh
@@ -3,4 +3,4 @@
 set -e
 
 # cargo clean
-WASM_BUILD_TYPE=release cargo run --features=with-setheum-runtime --features=on-chain-release-build -- build-spec --raw --chain setheum-latest > ./resources/setheum-dist.json
+WASM_BUILD_TYPE=release cargo run --features=with-setheum-runtime --features=on-chain-release-build -- build-spec --raw --chain setheum-latest > ./resources/chain_spec_mainnet_raw.json
