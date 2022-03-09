e89832cd518a1130841d128a148fcd1186bd5818
Muhammad-Jibril B.A <jbashir52@gmail.com>
2022-03-09 12:58:34 +0800

    Update dex_oracle.rs

--- lib-serml/defi/setswap/dex/src/tests.rs
+++ lib-serml/defi/setswap/dex/src/tests.rs
@@ -1,6 +1,6 @@
-// // This file is part of Acala.
+// // This file is part of Setheum.
 
-// // Copyright (C) 2020-2022 Acala Foundation.
+// // Copyright (C) 2020-2022 Setheum Labs.
 // // SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
 
 // // This program is free software: you can redistribute it and/or modify
