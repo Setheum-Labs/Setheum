fb3d60ce24770d42081b46be804bce6f74aea697
Muhammad-Jibril B.A. (Khalifa MBA) <jbashir52@gmail.com>
2024-03-14 02:39:16 +0800

    Add Rate Limiter

---
+++ rate-limiter/README.md
@@ -0,0 +1,5 @@
+# Setheum Rate Limiter
+
+## Overview
+
+This module Allows to limit access to some resource. Given a preferred rate (units of something) and last used amount of units of some resource, it calculates how long we should delay our next access to that resource in order to satisfy that rate.
---
+++ rate-limiter/src/rate_limiter.rs
@@ -0,0 +1,112 @@
+// بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيم
+
+// This file is part of Setheum.
+
+// Copyright (C) 2019-Present Setheum Labs.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use std::time::Instant;
+
+use futures::{future::BoxFuture, FutureExt};
+use log::trace;
+use tokio::{io::AsyncRead, time::sleep};
+
+use crate::{token_bucket::TokenBucket, LOG_TARGET};
+
+/// Allows to limit access to some resource. Given a preferred rate (units of something) and last used amount of units of some
+/// resource, it calculates how long we should delay our next access to that resource in order to satisfy that rate.
+pub struct SleepingRateLimiter {
+    rate_limiter: TokenBucket,
+}
+
+impl Clone for SleepingRateLimiter {
+    fn clone(&self) -> Self {
+        Self {
+            rate_limiter: self.rate_limiter.clone(),
+        }
+    }
+}
+
+impl SleepingRateLimiter {
+    /// Constructs a instance of [SleepingRateLimiter] with given target rate-per-second.
+    pub fn new(rate_per_second: usize) -> Self {
+        Self {
+            rate_limiter: TokenBucket::new(rate_per_second),
+        }
+    }
+
+    /// Given `read_size`, that is an amount of units of some governed resource, delays return of `Self` to satisfy configure
+    /// rate.
+    pub async fn rate_limit(mut self, read_size: usize) -> Self {
+        trace!(
+            target: LOG_TARGET,
+            "Rate-Limiter attempting to read {}.",
+            read_size
+        );
+
+        let now = Instant::now();
+        let delay = self.rate_limiter.rate_limit(read_size, now);
+
+        if let Some(delay) = delay {
+            trace!(
+                target: LOG_TARGET,
+                "Rate-Limiter will sleep {:?} after reading {} byte(s).",
+                delay,
+                read_size
+            );
+            sleep(delay).await;
+        }
+
+        self
+    }
+}
+
+/// Wrapper around [SleepingRateLimiter] to simplify implementation of the [AsyncRead](tokio::io::AsyncRead) trait.
+pub struct RateLimiter {
+    rate_limiter: BoxFuture<'static, SleepingRateLimiter>,
+}
+
+impl RateLimiter {
+    /// Constructs an instance of [RateLimiter] that uses already configured rate-limiting access governor
+    /// ([SleepingRateLimiter]).
+    pub fn new(rate_limiter: SleepingRateLimiter) -> Self {
+        Self {
+            rate_limiter: Box::pin(rate_limiter.rate_limit(0)),
+        }
+    }
+
+    /// Helper method for the use of the [AsyncRead](tokio::io::AsyncRead) implementation.
+    pub fn rate_limit<Read: AsyncRead + Unpin>(
+        &mut self,
+        read: std::pin::Pin<&mut Read>,
+        cx: &mut std::task::Context<'_>,
+        buf: &mut tokio::io::ReadBuf<'_>,
+    ) -> std::task::Poll<std::io::Result<()>> {
+        let sleeping_rate_limiter = match self.rate_limiter.poll_unpin(cx) {
+            std::task::Poll::Ready(rate_limiter) => rate_limiter,
+            _ => return std::task::Poll::Pending,
+        };
+
+        let filled_before = buf.filled().len();
+        let result = read.poll_read(cx, buf);
+        let filled_after = buf.filled().len();
+        let last_read_size = filled_after.saturating_sub(filled_before);
+
+        self.rate_limiter = sleeping_rate_limiter.rate_limit(last_read_size).boxed();
+
+        result
+    }
+}
